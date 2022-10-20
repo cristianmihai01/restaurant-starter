10f5634683772934737560f54a10ee4bffa8d624
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-10-20 16:43:30 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+// import components
+import Hero from './components/Hero';
+import About from './components/About';
+import Menu from './components/Menu';
+import Team from './components/Team';
+import Testimonial from './components/Testimonial';
+import Reservation from './components/Reservation';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div className='bg-red-300 h-full bg-pattern bg-repeat max-w-[1800px] mx-auto overflow-hidden'>
+      <Hero />
+      <About />
+      <Menu />
+      <Team />
+      <Testimonial />
+      <Reservation />
+      <Footer />
+      <div className='h-[530px] md:h-[380px]'></div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/TestimonialCarousel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TestimonialCarousel = () => {
+  return <div>TestimonialCarousel</div>;
+};
+
+export default TestimonialCarousel;
