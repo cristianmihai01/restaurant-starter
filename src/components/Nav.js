3abe8d7f45c2b40a1b5101647da5fd6ad3eaa96c
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-10-23 23:29:12 +0300

    added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,7 @@
 import React from 'react';
-// import data
-import { navData } from '../data';
 
 const Nav = () => {
-  return (
-    <nav className='w-full h-full'>
-      <ul className='h-full flex flex-col justify-center items-center gap-y-6'>
-        {navData.map((item, index) => {
-          return (
-            <li key={index}>
-              <a
-                className='text-xl capitalize font-primary italic hover:text-dark transition-all duration-300'
-                href={item.href}
-              >
-                {item.name}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
-  );
+  return <div>Nav</div>;
 };
 
 export default Nav;
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,23 +1,7 @@
 import React from 'react';
-// import data
-import { socialData } from '../data';
 
 const Socials = () => {
-  return (
-    <div className='flex gap-x-[10px]'>
-      {socialData.map((item, index) => {
-        return (
-          <a
-            className='border border-white/20  rounded-full w-[35px] h-[35px] flex items-center justify-center text-sm hover:text-accent hover:border-accent transition-all'
-            href={item.href}
-            key={index}
-          >
-            {item.icon}
-          </a>
-        );
-      })}
-    </div>
-  );
+  return <div>Socials</div>;
 };
 
 export default Socials;
